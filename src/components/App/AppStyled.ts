56399c08f37df1500dc95a4d071db1b1c9fed8ea
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-06-04 23:41:57 +0200

    Add App styles

---
+++ src/components/App/AppStyled.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const AppStyled = styled.div`
+  display: grid;
+  justify-content: space-evenly;
+  align-content: space-evenly;
+`;
+
+export default AppStyled;
