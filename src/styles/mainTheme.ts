b2c1287078af2b89290cae52f13b199cb0e7fb69
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-06-04 14:01:29 +0200

    Add main theme

---
+++ src/styles/mainTheme.ts
@@ -0,0 +1,13 @@
+import { DefaultTheme } from "styled-components";
+
+const mainTheme: DefaultTheme = {
+  colors: {
+    mainLight: "#898a85",
+    accent: "#2e74b3",
+  },
+  typography: {
+    mainFontFamily: "Roboto, sans-serif",
+  },
+};
+
+export default mainTheme;
