1c83d5048990c743667b9aeebf8458dffedeb174
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-06-06 17:34:56 +0200

    Add home link to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
 import HeaderStyled from "./HeaderStyled";
 
 const Header = (): React.ReactElement => {
-  return <HeaderStyled>Podcaster</HeaderStyled>;
+  return (
+    <HeaderStyled>
+      <Link to={"/"}>Podcaster</Link>
+    </HeaderStyled>
+  );
 };
 
 export default Header;
