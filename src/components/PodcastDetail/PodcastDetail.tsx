4995442fa94fc90dee898a6593cd947f08b59e91
Marta Ibarra <martaa.ibarra@hotmail.com>
2024-06-09 22:26:21 +0200

    Fix infinite loop bug

--- src/components/PodcastDetail/PodcastDetail.tsx
+++ src/components/PodcastDetail/PodcastDetail.tsx
@@ -1,77 +1,76 @@
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
-import { PodcastStructure } from "../../store/podcasts/types";
-import fetchPodcastById from "../../hooks/fetchPodcastById";
+import usePodcastDetailApi from "../../hooks/usePodcastById";
 import PodcastDetailStyled from "./PodcastDetailStyled";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { loadSelectedPodcastActionCreator } from "../../store/podcasts/podcastsSlice";
+import useLocalStorage from "../../hooks/useLocalStorage";
 
-const PodcastDetail = (): React.ReactElement => {
-  const { podcastId } = useParams<{ podcastId: string }>();
-  const [podcast, setPodcast] = useState<PodcastStructure | null>(null);
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<string | null>(null);
+export const PodcastDetail = (): React.ReactElement => {
+  const { podcastId } = useParams();
+  const { getPodcast } = useLocalStorage();
+  const { getPodcastById } = usePodcastDetailApi();
+  const dispatch = useAppDispatch();
+  const podcastDetails = useAppSelector(
+    (state) => state.podcastsState.selectedPodcast,
+  );
 
   useEffect(() => {
-    const getPodcast = async () => {
-      setLoading(true);
-
-      if (podcastId) {
-        try {
-          const fetchedPodcast = await fetchPodcastById(podcastId);
-
-          if (fetchedPodcast) {
-            setPodcast(fetchedPodcast);
-            setError(null);
-          } else {
-            setError("Podcast not found");
-          }
-        } catch (e) {
-          setError("Failed to fetch podcast details");
-        }
+    const fetchedPodcast = async () => {
+      const cachedPodcast = getPodcast(+podcastId!);
+      if (cachedPodcast) {
+        dispatch(loadSelectedPodcastActionCreator(cachedPodcast));
       } else {
-        setError("No podcast ID provided");
+        const podcastDetails = await getPodcastById(+podcastId!);
+        dispatch(loadSelectedPodcastActionCreator(podcastDetails));
       }
-      setLoading(false);
     };
 
-    getPodcast();
-  }, [podcastId]);
+    if (!podcastDetails || podcastDetails.collectionId !== +podcastId!) {
+      fetchedPodcast();
+    }
+  }, [getPodcastById, podcastId, dispatch, getPodcast, podcastDetails]);
 
-  if (loading) {
-    return <div>Your podcast is loading</div>;
-  }
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  if (!podcast) {
-    return <div>No podcast found</div>;
-  }
-
-  const {
-    summary,
-    "im:artist": artist,
-    "im:image": image,
-    "im:name": name,
-  } = podcast;
-
-  return (
+  return podcastDetails ? (
     <PodcastDetailStyled>
       <div className="detail-card">
         <img
           className="detail-card__image"
-          src={image[0]?.label || ""}
-          alt={name.label}
+          src={podcastDetails?.artworkUrl600}
+          alt={podcastDetails?.collectionName}
           width={140}
           height={140}
         />
         <div className="detail-card__credits">
-          <span className="detail-card__title">{name.label}</span>
-          <span className="detail-card__author">by {artist.label}</span>
+          <span className="detail-card__title">
+            {podcastDetails?.collectionName}
+          </span>
+          <span className="detail-card__author">
+            by {podcastDetails?.artistName}
+          </span>
         </div>
         <p className="detail-card__description">Description:</p>
-        <div className="detail-card__summary">{summary.label}</div>
+        <div className="detail-card__summary">
+          {podcastDetails?.collectionName}
+        </div>
+      </div>
+      <div>
+        <div className="episodes-count">
+          <span className="episodes-count__text">
+            Episodes: {podcastDetails.trackCount}{" "}
+          </span>
+        </div>
+        <div className="episodes-container">
+          <div className="episodes-container__info">
+            <div> Title</div>
+            <div>Date</div>
+            <div>Duration</div>
+          </div>
+        </div>
       </div>
     </PodcastDetailStyled>
+  ) : (
+    <></>
   );
 };
 
